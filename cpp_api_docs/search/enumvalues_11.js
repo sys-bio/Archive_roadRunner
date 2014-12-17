c284d452506b98993a09c01a57b4cbdfee5eecf7
AndySomogyi <andy.somogyi@gmail.com>
2014-12-17 15:57:49 -0500

    updates

---
+++ cpp_api_docs/search/enumvalues_11.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['variable_5fstep',['VARIABLE_STEP',['../classrr_1_1_integrator.html#a09e1fd0c86b6274b7e6667b501628fcfa3a3fd1ecae218b822423c4150adb9089',1,'rr::Integrator']]]
+];
