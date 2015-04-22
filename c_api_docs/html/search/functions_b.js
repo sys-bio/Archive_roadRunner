217b4ebe429c269e6eaa1a36c45dd1c5d6f1d022
0u812 <0u812@github.com>
2015-04-22 13:44:01 -0700

    adding initial C API docs

---
+++ c_api_docs/html/search/functions_b.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['reset',['reset',['../group__reset.html#gaeaf9374c9a0cad0898f5e0a37bc387c5',1,'rrc::reset()'],['../group__reset.html#gaeaf9374c9a0cad0898f5e0a37bc387c5',1,'reset():&#160;rrc_api.cpp']]],
+  ['resetall',['resetAll',['../group__reset.html#ga0fdde5a5cdd90408a0e2050d8e4243ec',1,'rrc::resetAll()'],['../group__reset.html#ga0fdde5a5cdd90408a0e2050d8e4243ec',1,'resetAll():&#160;rrc_api.cpp']]],
+  ['resettoorigin',['resetToOrigin',['../group__reset.html#ga30ad9eec8febc573febf1ad8b40c99ad',1,'rrc::resetToOrigin()'],['../group__reset.html#ga30ad9eec8febc573febf1ad8b40c99ad',1,'resetToOrigin():&#160;rrc_api.cpp']]],
+  ['rrcdatatostring',['rrCDataToString',['../group__to_string.html#ga505aeb10e21f0ef93770f7723aa12514',1,'rrc::rrCDataToString()'],['../group__to_string.html#ga505aeb10e21f0ef93770f7723aa12514',1,'rrCDataToString():&#160;rrc_utilities.cpp']]]
+];
