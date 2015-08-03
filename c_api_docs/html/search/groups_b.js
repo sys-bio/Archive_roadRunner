e4ef832799a78220ddcf7e2973c4f4d8ed09a4e1
0u812 <0u812@users.noreply.github.com>
2015-08-03 10:21:52 -0700

    update C doxy: add solver group

--- c_api_docs/html/search/groups_b.js
+++ c_api_docs/html/search/groups_b.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['solver_20options_20api',['Solver options API',['../group__simopts.html',1,'']]],
   ['steady_20state_20routines',['Steady state routines',['../group__steadystate.html',1,'']]],
   ['stochastic_20simulations',['Stochastic simulations',['../group__stochastic.html',1,'']]],
   ['stoichiometry_20analysis',['Stoichiometry analysis',['../group___stoich.html',1,'']]],
