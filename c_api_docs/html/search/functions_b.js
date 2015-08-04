ad56ee8a896754f0435e1b42ef9592f036fb5752
0u812 <0u812@users.noreply.github.com>
2015-08-04 16:34:36 -0700

    update docs

--- c_api_docs/html/search/functions_b.js
+++ c_api_docs/html/search/functions_b.js
@@ -3,6 +3,7 @@ var searchData=
   ['reset',['reset',['../group__reset.html#gaeaf9374c9a0cad0898f5e0a37bc387c5',1,'rrc::reset()'],['../group__reset.html#gaeaf9374c9a0cad0898f5e0a37bc387c5',1,'reset():&#160;_rrc_api.cpp']]],
   ['resetall',['resetAll',['../group__reset.html#ga0fdde5a5cdd90408a0e2050d8e4243ec',1,'rrc::resetAll()'],['../group__reset.html#ga0fdde5a5cdd90408a0e2050d8e4243ec',1,'resetAll():&#160;_rrc_api.cpp']]],
   ['resetcurrentintegratorparameters',['resetCurrentIntegratorParameters',['../group__simopts.html#gaefa42b6e122d13988bde99fcf6e050d6',1,'rrc::resetCurrentIntegratorParameters()'],['../group__simopts.html#gaefa42b6e122d13988bde99fcf6e050d6',1,'resetCurrentIntegratorParameters():&#160;rrc_api.cpp']]],
+  ['resetcurrentsteadystatesolverparameters',['resetCurrentSteadyStateSolverParameters',['../group__simopts.html#gafccd26495f4b099700e8f52fdb20794a',1,'rrc::resetCurrentSteadyStateSolverParameters()'],['../group__simopts.html#gafccd26495f4b099700e8f52fdb20794a',1,'resetCurrentSteadyStateSolverParameters():&#160;rrc_api.cpp']]],
   ['resettoorigin',['resetToOrigin',['../group__reset.html#ga30ad9eec8febc573febf1ad8b40c99ad',1,'rrc::resetToOrigin()'],['../group__reset.html#ga30ad9eec8febc573febf1ad8b40c99ad',1,'resetToOrigin():&#160;_rrc_api.cpp']]],
   ['rrcdatatostring',['rrCDataToString',['../group__to_string.html#ga505aeb10e21f0ef93770f7723aa12514',1,'rrc::rrCDataToString()'],['../group__to_string.html#ga505aeb10e21f0ef93770f7723aa12514',1,'rrCDataToString():&#160;rrc_utilities.cpp']]]
 ];
