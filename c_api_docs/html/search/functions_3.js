63675ef61b78ead4f948de9fc26fcd199864e140
0u812 <0u812@users.noreply.github.com>
2015-10-01 12:57:36 -0700

    update c docs

--- c_api_docs/html/search/functions_3.js
+++ c_api_docs/html/search/functions_3.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['enableloggingtoconsole',['enableLoggingToConsole',['../group__logging.html#ga1289bf0c4cc9c078d53d82b60e8dbdbb',1,'rrc::enableLoggingToConsole()'],['../group__logging.html#ga1289bf0c4cc9c078d53d82b60e8dbdbb',1,'enableLoggingToConsole():&#160;rrc_logging_api.cpp']]],
   ['enableloggingtofile',['enableLoggingToFile',['../group__logging.html#ga768513a67f7bb4faceedb0fd377d63eb',1,'rrc::enableLoggingToFile()'],['../group__logging.html#ga768513a67f7bb4faceedb0fd377d63eb',1,'enableLoggingToFile():&#160;rrc_logging_api.cpp']]],
+  ['enableloggingtofilewithpath',['enableLoggingToFileWithPath',['../group__logging.html#gacdb13a47cd0401735d81d0e88e5c13a0',1,'rrc::enableLoggingToFileWithPath()'],['../group__logging.html#gacdb13a47cd0401735d81d0e88e5c13a0',1,'enableLoggingToFileWithPath():&#160;rrc_logging_api.cpp']]],
   ['evalmodel',['evalModel',['../group__state.html#ga8f5b221fd8d82bc44f43f2742122f91a',1,'rrc::evalModel()'],['../group__state.html#ga8f5b221fd8d82bc44f43f2742122f91a',1,'evalModel():&#160;_rrc_api.cpp']]]
 ];
